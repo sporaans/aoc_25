0ac9d35fd82aed073bb494b1d725c3b92dfaf42b
Davis <davis@flowertech.se>
2025-12-02 10:47:12 +0100

    day 2 task 1

---
+++ src/days/day1.rs
@@ -0,0 +1,86 @@
+pub fn task1(input: &str) -> Result<i32, Box<dyn std::error::Error>>{
+    let rotations: Vec<&str> = input.trim().split('\n').collect();
+    let mut position = 50;
+    let mut zeros = 0;
+
+    for (_,rotation) in rotations.iter().enumerate() {
+        let clicks_raw: i32 = rotation[1..].parse()?;
+        let clicks = clicks_raw % 100;
+        let direction = rotation.chars().next();
+
+        match direction {
+            Some('R') => position = position + clicks,
+            Some('L') => position = position - clicks,
+            _ => return Err(format!("unknown direction: {}", direction.unwrap_or('\0')).into()),
+        }
+
+        if position >= 100 {
+            position = position - 100;
+        }
+
+        if position < 0 {
+            position = 100 - position.abs();
+        }
+
+        if position == 0 {
+            zeros = zeros + 1;
+        }
+
+        // if i < 50 {
+        //     println!("rotation: {}, clicks: {}, position: {}", rotation, clicks_raw, position);
+        // }
+    }
+
+    Ok(zeros)
+}
+
+pub fn task2(input: &str) -> Result<i32, Box<dyn std::error::Error>>{
+    let rotations: Vec<&str> = input.trim().split('\n').collect();
+    // let rotations: Vec<&str> = vec!["L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82"];
+    // let rotations: Vec<&str> = vec!["R100"];
+    let mut position = 50;
+    let mut zeros = 0;
+
+    for (i,rotation) in rotations.iter().enumerate() {
+        if i < 100 {
+            print!("start: {} ", position);
+        }
+        let start_position = position;
+
+        let clicks: i32 = rotation[1..].parse()?;
+        let direction = rotation.chars().next();
+
+        match direction {
+            Some('R') => position = position + clicks,
+            Some('L') => position = position - clicks,
+            _ => return Err(format!("unknown direction: {}", direction.unwrap_or('\0')).into()),
+        }
+        zeros = zeros + (position / 100).abs();
+
+        // not even a mother can love this
+        if position < 0 {
+            position = position % 100;
+            if position != 0 {
+                if start_position != 0 {
+                    zeros = zeros + 1;
+                }
+                position = 100 - position.abs();
+            } else {
+                if start_position != 0 || clicks == 0 {
+                    zeros = zeros + 1;
+                }
+            }
+        } else {
+            if position == 0 {
+                zeros = zeros + 1;
+            }
+            position = position % 100;
+        }
+
+        if i < 100 {
+            println!("rotation: {}, position: {}, zeros: {}", rotation, position, zeros);
+        }
+    }
+
+    Ok(zeros)
+}
---
+++ src/days/day2.rs
@@ -0,0 +1,28 @@
+pub fn task1(input: &str) -> Result<i64, Box<dyn std::error::Error>> {
+    let id_ranges: Vec<&str> = input.trim().split(',').collect();
+    let mut result= 0;
+
+    for id_range in id_ranges {
+        let (start_str,end_str) = id_range.split_once("-").ok_or("range cannot be split")?;
+        let start: i64 = start_str
+            .parse()
+            .map_err(|e| format!("could not parse '{}': {}", start_str, e))?;
+        let end: i64 = end_str
+            .parse()
+            .map_err(|e| format!("could not parse '{}': {}", end_str, e))?;
+
+        for id in start..=end {
+            let id_str = id.to_string();
+            if id_str.len() % 2 != 0 {
+                continue
+            } 
+            let (first,second) = id_str.split_at(id_str.len()/2);
+
+            if first == second {
+                result += id;
+            }
+        }
+    }
+
+    Ok(result)
+}
